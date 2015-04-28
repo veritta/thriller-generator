b7e6df897bb1c775da6e4d3cb8c58f04e77919d4
veritta <arterialist@gmail.com>
2015-04-28 22:12:43 +0200

    added options and new format

--- generator.js
+++ generator.js
@@ -1,21 +1,10 @@
 var options = {
-	"objectNouns": ["Conspiracy", "Secrets", "Ghosts", "Assassins", "Scrolls", "Codex", "Agents"],
-	"properNouns": ["Atlantis", "Cairo", "Moscow", "Versailles", "Venice", "Shanghai", "Beijing", "Stonehenge", "Poseidon"],
-	"characters" : ["Scarlet", "Rook", "Watchman", "Soldier"],
-	"possessions": ["Gambit", "Winter"],
+	"objectNouns": ["Conspiracy", "Secrets", "Ghosts", "Assassins", "Scrolls", "Codex", "Agents", "Protocol", "Storm"],
+	"properNouns": ["Atlantis", "Cairo", "Moscow", "Versailles", "Venice", "Shanghai", "Beijing", "Romanov", "Tudor"],
+	"characters" : ["Rook", "Watchman", "Soldier", "Guard", "Spymaster"],
+	"possessions": ["Gambit", "Winter", "Protocol", "Vow", "Oath"],
+	"adjectives" : ["Forgotten", "Lost", "Last"]
 };
-
-var formats = [
-	function() {
-		return "The " + generate.objectNoun() + " of " + generate.properNoun();
-	},
-	function() {
-		return "The " + generate.properNoun() + " " + generate.objectNoun();
-	},
-	function() {
-		return generate.character() + "'s " + generate.possession();
-	},
-];
 
 var generate = {
 	"objectNoun": function() {
@@ -30,7 +19,28 @@ var generate = {
 	"possession": function() {
 		return randomlySelect(options.possessions);
 	},
+	"adjective" : function() {
+		return randomlySelect(options.adjectives);
+	},
 };
+
+var formats = [
+	function() {
+		return "The " + generate.objectNoun() + " of " + generate.properNoun();
+	},
+	function() {
+		return "The " + generate.properNoun() + " " + generate.objectNoun();
+	},
+	function() {
+		return generate.character() + "'s " + generate.possession();
+	},
+	function() {
+		return "The " + generate.character() + " of " + generate.properNoun();
+	},
+	function() {
+		return "The " + generate.adjective() + " " + generate.possession() + " of " + generate.properNoun();
+	},
+];
 
 
 function randomlySelect(array) {
@@ -55,6 +65,11 @@ function generateActionThriller() {
 
 console.log('-------------------------------\nACTION THRILLER GENERATOR\n-------------------------------');
 
-console.log(generateActionThriller());
+for (var i = 0; i < formats.length; i++) {
+	var format = formats[i];
+	console.log(format());
+};
+
+console.log('\n' + generateActionThriller());
 
 console.log('\n\n');
